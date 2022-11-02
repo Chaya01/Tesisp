d4d705895a7f71c3a568d8d1e4d2ac1cc85afa2b
Kevin <kevinaroca@alu.unach.cl>
2022-11-02 20:20:03 -0300

    modelo de inscripcion

--- tesis/artemis/admin.py
+++ tesis/artemis/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-from .models import Estudiantes, Cursos, Diplomados, Areas,Becas
+from .models import Estudiantes, Cursos, Diplomados, Areas,Becas, Docentes, Profesiones, Inscripciones
 
 admin.site.register(Estudiantes)
 admin.site.register(Cursos)
 admin.site.register(Diplomados)
 admin.site.register(Areas)
 admin.site.register(Becas)
+admin.site.register(Docentes)
+admin.site.register(Profesiones)
+admin.site.register(Inscripciones)
 
 # Register your models here.
--- tesis/artemis/models.py
+++ tesis/artemis/models.py
@@ -7,15 +7,21 @@ from django.db import models
 class Becas(models.Model):
     id = models.AutoField(primary_key=True)
     nombre_beca = models.CharField(max_length=20)
+    def __str__(self):
+        return self.nombre_beca
 
 class Areas(models.Model):
     id = models.AutoField(primary_key=True)
     nombre_area = models.CharField(max_length=20)
+    def __str__(self):
+        return self.nombre_area
 
 class Profesiones(models.Model):
     id = models.AutoField(primary_key=True)
     nombre_profesion = models.CharField(max_length=20)
     area_profesion= models.ForeignKey(Areas, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.nombre_profesion
 
 class Estudiantes(models.Model):
     rut_estudiante = models.CharField(max_length=20,primary_key=True)
@@ -25,6 +31,8 @@ class Estudiantes(models.Model):
     direccion_estudiante = models.CharField(max_length=30)
     profesion = models.ForeignKey(Profesiones, on_delete= models.CASCADE)
     beca = models.ForeignKey(Becas, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.nombre_estudiante
 
 class Docentes(models.Model):
     rut_docente = models.CharField(primary_key=True,max_length=20)
@@ -32,14 +40,30 @@ class Docentes(models.Model):
     apellido_docente = models.CharField(max_length=20)
     profesion_docente = models.ForeignKey(Profesiones,on_delete=models.CASCADE)
     num_tel_docente = models.IntegerField()
+    def __str__(self):
+        return self.nombre_docente
 
 class Cursos(models.Model):
     id = models.AutoField(primary_key=True)
     nombre_curso = models.CharField(max_length=20)
     docente = models.ForeignKey(Docentes, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.nombre_curso
 
 class Diplomados(models.Model):
     id = models.AutoField(primary_key=True)
     nombre_diplomado = models.CharField(max_length=20)
-    cursos_req = models.ForeignKey(Cursos, on_delete=models.CASCADE)
+    # cursos_req = models.ForeignKey(Cursos, on_delete=models.CASCADE)
+    cursos_req = models.ManyToManyField(Cursos)
     capacidad = models.IntegerField()
+    def __str__(self):
+        return self.nombre_diplomado
+
+class Inscripciones(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre_inscripcion = models.CharField(max_length=20)
+    diplomado = models.ManyToManyField(Diplomados)
+    capacidad = models.IntegerField()
+    estudiantes = models.ManyToManyField(Estudiantes)
+    def __str__(self):
+        return self.nombre_inscripcion
